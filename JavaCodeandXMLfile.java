765a83d7647a41809f09586ec0b745b1c5bd8f6a
ALU-RWA <43107201+ALU-RWA@users.noreply.github.com>
2020-02-14 10:02:31 +0200

    Update JavaCodeandXMLfile.java

--- JavaCodeandXMLfile.java
+++ JavaCodeandXMLfile.java
@@ -1,6 +1,5 @@
 # Shared-Preferences-Example
 This is an example of an app that uses Shared Preferences. When a user enters the login details, after closing the app and the user want to access the app again, the login details will have been saved.
-
 package com.example.sharedpreference;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +10,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText ed1,ed2,ed3;
     Button b1;
+    TextView name, phone, email;
 
     public static final String MyPREFERENCES = "MyPrefs" ;
     public static final String Name = "nameKey";
@@ -35,23 +36,36 @@ public class MainActivity extends AppCompatActivity {
 
         b1=(Button)findViewById(R.id.button);
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-
+         GetSavedData();
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String n  = ed1.getText().toString();
-                String ph  = ed2.getText().toString();
-                String e  = ed3.getText().toString();
-
-                SharedPreferences.Editor editor = sharedpreferences.edit();
-
-                editor.putString(Name, n);
-                editor.putString(Phone, ph);
-                editor.putString(Email, e);
-                editor.apply();
+               saveData();
                 Toast.makeText(MainActivity.this,"Content Saved",Toast.LENGTH_LONG).show();
             }
         });
+    }
+    public void saveData(){
+        String n  = ed1.getText().toString();
+        String ph  = ed2.getText().toString();
+        String e  = ed3.getText().toString();
+
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+
+        editor.putString(Name, n);
+        editor.putString(Phone, ph);
+        editor.putString(Email, e);
+        editor.commit();
+    }
+    public void GetSavedData(){
+        if(sharedpreferences.contains(Name)){
+            String tvName = sharedpreferences.getString(Name,"");
+            String tvEmail = sharedpreferences.getString(Email,"");
+            String tvPhoneNo = sharedpreferences.getString(Phone,"");
+            ed1.setText(tvName);
+            ed2.setText(tvEmail);
+            ed3.setText(tvPhoneNo);
+        }
     }
 
 }
